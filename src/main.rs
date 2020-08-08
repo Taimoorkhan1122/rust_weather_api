2985a35bfb9859f0ec8100d3708babf997d32d4c
Taimoor khan <khant8k83@gmail.com>
2020-08-08 17:42:16 +0500

    Working Weather api

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use std::io;
 use async_std::task;
+use serde_json::Value;
 use surf;
+
 
 async fn fetch(url: &str) -> Result<String, surf::Exception> {
     surf::get(url).recv_string().await
@@ -7,17 +10,37 @@ async fn fetch(url: &str) -> Result<String, surf::Exception> {
 
 async fn execute() {
 
-    let city_name = "Karachi";
-    let api_key = "3770c89821b6000981072f41e3ec419f"
-    let mut api = format!("api.openweathermap.org/data/2.5/weather?q={}&appid={",
-        city_name ,api_key})
+    //taking user parameters
+    let mut city_name = String::new();
+    println!("Enter city name");
+    io::stdin().read_line(&mut city_name)
+    .expect("Sorry can't read that");
 
-    println!("{}"j, api);
+    let city_name = city_name.trim();
 
-    // match fetch("").await {
-    //     Ok(s) =>
-    //     Err(e) =>
-    // }
+    let api_key = "3770c89821b6000981072f41e3ec419f";
+    let api = format!("https://api.openweathermap.org/data/2.5/weather?q={}&appid={}",
+        city_name ,api_key);
+
+    // println!("{}", api);
+    // let res = fetch(&api).await;
+    // let json = serde_json::Value::String(serde_json::to_string(res.as_str()).unwrap());
+
+    match fetch(&api).await {
+        Ok(s) => {
+            // serde_json::Value::String(serde_json::to_string(s.as_str()).unwrap())
+            // let rjson: Value = serde_json::Value::String(serde_json::to_string(&s).unwrap()); 
+
+            let json_str: Value = serde_json::from_str(s.as_str()).unwrap();
+            if json_str["cod"] == 200 {
+                println!("Weather now in {}\nDetails: {}",json_str["name"],json_str["weather"][0] );
+            } else {
+                println!("{}", json_str["message"]);
+            }
+        },
+        Err(e) => println!("Error in fetching data: {}", e),
+    };
+
 }
 
 fn main() {
